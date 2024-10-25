9c372318dc2243e9da80588b4c990cc8b4f8ea66
Ewan Short <eshort0401@gmail.com>
2024-10-25 23:53:14 +1100

    lower resolution of figures produced on GADI

--- workflow/gridrad_gadi/gridrad_PBS.sh
+++ workflow/gridrad_gadi/gridrad_PBS.sh
@@ -4,16 +4,15 @@
 #PBS -l ncpus=8
 #PBS -l mem=32GB
 #PBS -l jobfs=2GB
-#PBS -l walltime=12:00:00
+#PBS -l walltime=3:00:00
 #PBS -l wd
 #PBS -r y
 #PBS -J 11-20
 #PBS -l storage=gdata/rt52+gdata/w40+gdata/rq0+scratch/w40
-#PBS -e /home/563/esh563/THOR/workflow/gridrad_gadi/PBS_jobs/gridrad_${PBS_JOBID}_${PBS_ARRAY_INDEX}.e
-#PBS -o /home/563/esh563/THOR/workflow/gridrad_gadi/PBS_jobs/gridrad_${PBS_JOBID}_${PBS_ARRAY_INDEX}.o
 
 # Load conda and activate the THOR environment
 module load python3/3.10.4
+conda init
 conda activate THOR
 
 # Read the directories from the file created by the gridrad_job.sh script
