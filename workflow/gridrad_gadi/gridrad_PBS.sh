271edb6c36f1857df8ff80a65d3afa8309d6f938
Ewan Short <eshort0401@gmail.com>
2024-10-26 15:41:52 +1100

    switch to gnu parallel

--- workflow/gridrad_gadi/gridrad_PBS.sh
+++ workflow/gridrad_gadi/gridrad_PBS.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #PBS -q normalbw
 #PBS -P w40
-#PBS -l ncpus=8
-#PBS -l mem=32GB
+#PBS -l ncpus=16
+#PBS -l mem=64GB
 #PBS -l jobfs=2GB
 #PBS -l walltime=3:00:00
 #PBS -l wd
-#PBS -r y
-#PBS -J 11-20
 #PBS -l storage=gdata/rt52+gdata/w40+gdata/rq0+scratch/w40
 
+# Load gnu-parallel
+module load parallel
 # Load conda and activate the THOR environment
 module load python3/3.10.4
 conda init
@@ -17,5 +17,10 @@ conda activate THOR
 
 # Read the directories from the file created by the gridrad_job.sh script
 directories=($(cat "${filepath}"))
+script="/home/563/esh563/THOR/workflow/gridrad_gadi/gridrad.py"
+
+# For testing: Use only the first 5 entries of directories
+test_directories=("${directories[@]:10:15}")
+
 # Run the gridrad.py script with the directory corresponding to the current PBS_ARRAY_INDEX
-python3 /home/563/esh563/THOR/workflow/gridrad_gadi/gridrad.py ${directories[$PBS_ARRAY_INDEX-1]}
+parallel -j 2 python3 $script ::: "${test_directories[@]}"
--- workflow/gridrad_gadi/gridrad_job.sh
+++ workflow/gridrad_gadi/gridrad_job.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+year=${1:-2010}
+# OUTPUT_DIR="/scratch/w40/esh563/THOR_output"
+# THOR_DIR="/home/563/esh563/THOR"
+OUTPUT_DIR="/home/ewan/THOR_output"
+THOR_DIR="/home/ewan/Documents/THOR"
 
-DATA_DIR="/scratch/w40/esh563/THOR_output/input_data/raw/d841006/volumes/2010"
-SCRIPT_DIR="/home/563/esh563/THOR/workflow/gridrad_gadi"
-# DATA_DIR="/home/ewan/THOR_output/input_data/raw/d841006/volumes/2010"
-# SCRIPT_DIR="/home/ewan/Documents/THOR/workflow/gridrad_gadi"
+DATA_DIR="${OUTPUT_DIR}/input_data/raw/d841006/volumes/${YEAR}"
+SCRIPT_DIR="${THOR_DIR}/workflow/gridrad_gadi"
 directories=$(find ${DATA_DIR} -mindepth 1 -type d -print | sort)
-filepath="${SCRIPT_DIR}/directories.txt"
+filepath="${SCRIPT_DIR}/${year}_directories.txt"
 echo ${directories} > ${filepath}
 
-qsub -v filepath=${filepath} ./gridrad_PBS.sh
+# qsub -v filepath=${filepath} ./gridrad_PBS.sh
