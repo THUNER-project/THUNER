ea00c869df743477e61b5c834564ddbe2e006dfd
Ewan Short <eshort0401@gmail.com>
2025-02-26 12:27:11 +1100

    add mdss get scripts

--- workflow/gridrad_severe_gadi/mdss_year_get.sh
+++ workflow/gridrad_severe_gadi/mdss_year_get.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 year=${1:-2010}
-LOCAL_DIR=/scratch/w40/esh563/THUNER_output/input_data/archive/d841006/volumes/${year}
+LOCAL_DIR=/scratch/w40/esh563/THUNER_output/input_data/raw/d841006/volumes/${year}
 directories=$(ls ${LOCAL_DIR})
 DEST_DIR=esh563/d841006/volumes/${year}
 mdss mkdir ${DEST_DIR}
 for event in ${directories}; do
-    qsub -v year=${year},event=${event} mdss_event_put_PBS.sh
+    qsub -v year=${year},event=${event} mdss_event_get_PBS.sh
 done
