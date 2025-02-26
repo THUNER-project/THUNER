988adea9239faccc1815a16f3c994b7449896c91
Ewan Short <eshort0401@gmail.com>
2025-02-26 13:35:12 +1100

    amend get put

--- workflow/gridrad_severe_gadi/mdss_year_put.sh
+++ workflow/gridrad_severe_gadi/mdss_year_put.sh
@@ -4,6 +4,10 @@ LOCAL_DIR=/scratch/w40/esh563/THUNER_output/input_data/archive/d841006/volumes/$
 directories=$(ls ${LOCAL_DIR})
 DEST_DIR=esh563/d841006/volumes/${year}
 mdss mkdir ${DEST_DIR}
+LOG_DIR=/scratch/w40/esh563/THUNER_output/PBS_logs/put_${year}
+mkdir -p ${LOG_DIR}
+cd ${LOG_DIR}
+SCRIPT_DIR=/home/esh563/THUNER/workflow/gridrad_severe_gadi
 for event in ${directories}; do
-    qsub -v year=${year},event=${event} mdss_event_put_PBS.sh
+    qsub -v year=${year},event=${event} ${SCRIPT_DIR}/mdss_event_put_PBS.sh
 done
