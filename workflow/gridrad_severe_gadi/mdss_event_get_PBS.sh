15042063248df327db0846043b3567159ab1b2ab
Ewan Short <eshort0401@gmail.com>
2025-02-27 15:12:08 +1100

    amend mdss get

--- workflow/gridrad_severe_gadi/mdss_event_get_PBS.sh
+++ workflow/gridrad_severe_gadi/mdss_event_get_PBS.sh
@@ -7,3 +7,5 @@
 LOCAL_DIR=/scratch/w40/esh563/THUNER_output/input_data/raw/d841006/volumes/${year}/${event}
 DEST_DIR=esh563/d841006/volumes/${year}/${event}
 mdss get -r ${DEST_DIR} ${LOCAL_DIR}
+tar -xzvf ${LOCAL_DIR}
+rm ${LOCAL_DIR}
