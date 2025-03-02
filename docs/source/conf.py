c59eafeefa61702c82fc12f36c2c02165b0712da
Ewan Short <eshort0401@gmail.com>
2025-03-02 16:30:23 +1100

    amend conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 
+# First add the project root to the path
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+
 # -- Project information
 
 project = "THUNER"
